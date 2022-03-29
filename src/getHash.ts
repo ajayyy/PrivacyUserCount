02e8e0a82531ff4fdd8c12701ea8f77ea6918fbe
Ajay Ramachandran <dev@ajay.app>
2022-03-29 13:15:01 -0400

    Change hash

--- src/getHash.ts
+++ src/getHash.ts
@@ -1,6 +1,6 @@
 import crypto from 'crypto';
 
 export default (value: any) => {
-  let hashCreator = crypto.createHash('sha256');
+  let hashCreator = crypto.createHash('sha1');
   return hashCreator.update(value).digest('hex');
 }
